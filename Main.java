80c21b7d20627476f934221af0cd82d5120b142b
FazlullahBokhari <fazlullahb@gmail.com>
2024-12-08 15:14:02 +0530

    Added one more

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hi Faiz");
         System.out.println("From Java Project Fazlu");
+        Sysytem.out.println("Added one more");
     }
 }
